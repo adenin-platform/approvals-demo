128c891490ce15c831789305cd95cd91b8f66158
olliejm <olliejm@protonmail.com>
2019-06-17 13:55:52 +0100

    internal script full

--- script/script.js
+++ script/script.js
@@ -3,16 +3,20 @@
 module.exports = async (activity) => {
   try {
     switch (activity.Request.Path) {
+    // approve action callback
     case 'approve':
       approve();
       break;
+    // reject action callback
     case 'reject':
       reject();
       break;
+    // request of approvals list
     default:
       generate();
     }
   } catch (error) {
+    // standard error handling
     let m = error.message;
 
     if (error.stack) m = m + ': ' + error.stack;
@@ -24,38 +28,94 @@ module.exports = async (activity) => {
   }
 
   function approve() {
-    activity.Response.Data.success = true;
+    // fetch the ID of the approved item
+    const id = activity.Request.Data.model.id;
+
+    // here we would implement logic to approve the item with above ID in an API call
+
+    // send a success response
+    activity.Response.Data = {
+      id: id,
+      success: true
+    };
   }
 
   function reject() {
-    activity.Response.Data.success = true;
+    // fetch the ID of the rejected item
+    const id = activity.Request.Data.model.id;
+
+    // here we would implement logic to approve the item with above ID in an API call
+
+    // send a success response
+    activity.Response.Data = {
+      id: id,
+      success: true
+    };
   }
 
   function generate() {
-    let p = parseInt(activity.Request.Query.page) || 1;
-    let n = parseInt(activity.Request.Query.pageSize) || 20;
+    // Find the pagination parameters from request, or set defaults
+    let action = 'firstpage';
+    let page = parseInt(activity.Request.Query.page) || 1;
+    let pageSize = parseInt(activity.Request.Query.pageSize) || 20;
 
+    // if it is a request for the next page, fetch those values (or defaults)
     if (
       activity.Request.Data &&
       activity.Request.Data.args &&
-      activity.Request.Data.args._page &&
-      activity.Request.Data.args._pageSize
+      activity.Request.Data.args.atAgentAction === 'nextpage'
     ) {
-      p = parseInt(activity.Request.Data.args._page) || 2;
-      n = parseInt(activity.Request.Data.args._pageSize) || 20;
+      action = 'nextpage';
+      page = parseInt(activity.Request.Data.args._page) || 2;
+      pageSize = parseInt(activity.Request.Data.args._pageSize) || 20;
     }
+
+    // correct any invalid pagination values
+    if (page < 0) page = 1;
+    if (pageSize < 1 || pageSize > 99) pageSize = 20;
 
     const items = [];
 
-    for (let i = (p - 1) * n; i < n; i++) {
-      const id = (p * 100) + i + 1;
+    // loop from first to last index of current page
+    for (let i = (page - 1) * pageSize; i < page * pageSize; i++) {
+      // genarate some random items, where we would normally fetch from API
+      const id = (page * 100) + i + 1 - ((page - 1) * pageSize);
 
       items.push({
         id: id,
-        title: `approval ${id}`
+        title: `approval ${id}`,
+        description: 'PTO Request',
+        date: new Date()
       });
     }
 
+    // attach items to the response
     activity.Response.Data.items = items;
+
+    // Title of card
+    activity.Response.Data.title = 'My Approvals';
+
+    // Footer link and label
+    activity.Response.Data.link = 'https://my-approvals.app/';
+    activity.Response.Data.linkLabel = 'All Approvals';
+
+    // capture how many approvals we have
+    const value = items.length;
+
+    // card isn't actionable if we have no approvals
+    activity.Response.Data.actionable = value > 0;
+
+    // set description text for initial overview
+    if (value > 0) {
+      activity.Response.Data.value = value;
+      activity.Response.Data.description = value > 1 ? `You have ${value} approvals` : 'You have 1 approval';
+    } else {
+      activity.Response.Data.description = 'You have no approvals.';
+    }
+
+    // let the card know we want the next page next time, and current position
+    activity.Response.Data._action = action;
+    activity.Response.Data._page = page;
+    activity.Response.Data._pageSize = pageSize;
   }
 };
